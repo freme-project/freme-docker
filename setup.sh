b7b76c23fb741bc849c638ccc999ca22d5a6a8c4
Sandro <sandroacoelho@gmail.com>
2016-07-30 20:11:07 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,17 +6,23 @@ echo "--------------------------------------------------------------------------
 echo "Welcome to FREME NER setup"
 echo "---------------------------------------------------------------------------"
 
+# Common config
+apt-get update
+apt-get upgrade
+apt-get install -y wget curl csvtool gawk git bzip2
+
 # Debian Jessie Config
 echo "deb http://http.debian.net/debian wheezy-backports main" >  /etc/apt/sources.list.d/backports.list
 echo "deb https://apt.dockerproject.org/repo debian-jessie main" >  /etc/apt/sources.list.d/docker.list
 
 # Common config (Docker, curl, wget , csvtool, gawk, git and bzip2)
 apt-get update
+apt-get install -y apt-transport-https ca-certificates
 apt-get purge "lxc-docker*"
 apt-get purge "docker.io*"
 apt-cache policy docker-engine
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-apt-get install -y wget curl csvtool gawk git bzip2 apt-transport-https ca-certificates docker-engine
+apt-get install -y docker-engine
 
 #Docker compose
 curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
