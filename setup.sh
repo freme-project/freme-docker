6717aacba49254e309be10a075180da8ca671449
Sandro <sandroacoelho@gmail.com>
2016-07-30 20:06:38 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,9 +15,8 @@ apt-get update
 apt-get purge "lxc-docker*"
 apt-get purge "docker.io*"
 apt-cache policy docker-engine
-apt-get install -y wget curl csvtool gawk git bzip2 apt-transport-https ca-certificates docker-engine
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-
+apt-get install -y wget curl csvtool gawk git bzip2 apt-transport-https ca-certificates docker-engine
 
 #Docker compose
 curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
