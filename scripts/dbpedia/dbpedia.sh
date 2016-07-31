6390a5c3d0568e49306a0d566bc1833bf1c530f6
Sandro <sandroacoelho@gmail.com>
2016-07-31 16:30:14 -0300

    Update dbpedia.sh

--- scripts/dbpedia/dbpedia.sh
+++ scripts/dbpedia/dbpedia.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-readonly DBPEDIA_VERSION=$1
+readonly DBPEDIA_URL="http://downloads.dbpedia.org/2015-10"
 readonly all_languages=(en fr es nl ru)
 
 for lang in ${all_languages[@]}
 do
-  wget "http://downloads.dbpedia.org/2015-10/core-i18n/${lang}/labels_${lang}.ttl.bz2"
+  echo "Downloading labels for ${lang}"
+  wget "${DBPEDIA_URL}/core-i18n/${lang}/labels_${lang}.ttl.bz2"
   bzip2 -d "labels_${lang}.ttl.bz2"
 done
 
+echo "done!"
