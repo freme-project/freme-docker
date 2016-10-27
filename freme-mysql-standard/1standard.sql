8f3cee8e8aeb9484864cfae7518604163cd6d3e4
Jan Nehring <gmnoname@gmx.de>
2016-10-27 13:56:29 +0200

    initialize docker with data

---
+++ freme-mysql-standard/1standard.sql
@@ -0,0 +1,5 @@
+use `freme`;
+
+INSERT INTO dataset_metadata (visibility, owner_name, description, name, total_entities, creation_time) 
+VALUES ('1', 'admin', 'DBpedia datasets for all languages', 'dbpedia', '12382158',0),
+('2', 'admin', 'Europeana.eu is an internet portal that acts as an interface to millions of books, paintings, films, museum objects and archival records that have been digitised throughout Europe.', 'europeana', '1431968',0);
